87522c0ad80c028737b153a3a782087a8391c9b4
BlastFM Limited <30941076+BlastFM@users.noreply.github.com>
2021-11-09 21:08:12 +0000

    Update ssl_renewal_check.php
    Cleaned up the code a bit and removed variables that were serving no purpose.

--- ssl_renewal_check.php
+++ ssl_renewal_check.php
@@ -1,20 +1,17 @@
 <?php
-$url = "https://google.com";
-$orignal_parse = parse_url($url, PHP_URL_HOST);
-$get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
-$read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
-$cert = stream_context_get_params($read);
-$certinfo = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
-$valid_from = date(DATE_RFC2822,$certinfo['validFrom_time_t']);
-$valid_to = date(DATE_RFC2822,$certinfo['validTo_time_t']);
-echo $url." - SSL Certificate Valid From: ".$valid_from.PHP_EOL;
-echo $url." - SSL Certificate Valid To: ".$valid_to.PHP_EOL;
-if($certinfo['validTo_time_t'] <= time()) {
-session_write_close();
-exec("certbot renew");
-session_start();
-echo "One or more certificates were renewed.".PHP_EOL;
+    $url = "https://google.com";
+    $orignal_parse = parse_url($url, PHP_URL_HOST);
+    $get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
+    $read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
+    $cert = stream_context_get_params($read);
+    $certinfo = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
+	echo $url." - SSL Certificate Valid From: ".date(DATE_RFC2822, $certinfo['validFrom_time_t']).PHP_EOL;
+	echo $url." - SSL Certificate Valid To: ".date(DATE_RFC2822, $certinfo['validTo_time_t']).PHP_EOL;
+if ($certinfo['validTo_time_t'] <= time() OR $certinfo['validFrom_time_t'] >= time()) {
+	session_write_close();
+	exec("certbot renew");
+	echo "One or more SSL certificates were renewed.".PHP_EOL;
 } else {
-echo "No certificates due for renewal.".PHP_EOL;
+	echo "No SSL certificates are due for renewal today.".PHP_EOL;
 }
 ?>
