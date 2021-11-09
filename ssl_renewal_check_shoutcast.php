070d8cb84e0ae9bd9031a3c651b3cb861875a25e
BlastFM Limited <30941076+BlastFM@users.noreply.github.com>
2021-11-09 21:09:48 +0000

    Create ssl_renewal_check_shoutcast.php

---
+++ ssl_renewal_check_shoutcast.php
@@ -0,0 +1,17 @@
+<?php
+    $url = "https://google.com";
+    $orignal_parse = parse_url($url, PHP_URL_HOST);
+    $get = stream_context_create(array("ssl" => array("capture_peer_cert" => TRUE)));
+    $read = stream_socket_client("ssl://".$orignal_parse.":443", $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $get);
+    $cert = stream_context_get_params($read);
+    $certinfo = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
+	echo $url." - SSL Certificate Valid From: ".date(DATE_RFC2822, $certinfo['validFrom_time_t']).PHP_EOL;
+	echo $url." - SSL Certificate Valid To: ".date(DATE_RFC2822, $certinfo['validTo_time_t']).PHP_EOL;
+if ($certinfo['validTo_time_t'] <= time() OR $certinfo['validFrom_time_t'] >= time()) {
+	session_write_close();
+	exec("certbot renew;killall sc_serv;sleep 3;cd /etc/shoutcast;./sc_serv &");
+	echo "One or more SSL certificates were renewed.".PHP_EOL;
+} else {
+	echo "No SSL certificates are due for renewal today.".PHP_EOL;
+}
+?>
